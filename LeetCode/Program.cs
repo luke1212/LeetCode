b29fe09c269f52d7b6e4a25aa2589ea527109f4d
Luke_Sun <luke890602@gmail.com>
2019-02-15 15:19:35 -0600

    problem3

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LeetCode
 {
@@ -6,14 +7,35 @@ namespace LeetCode
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var s = "hello,world";
+            var s1 = s.Remove(',');
+            foreach (var s2 in s1)
+            {
+                Console.WriteLine(s2);
+            }
         }
 
         // problem 3
         public string ToLowerCase(string str)
-            {
-                return str.ToLower();
-            }
+        {
+            return str.ToLower();
+        }
         //this is just a test
+
+        public int NumUniqueEmails(string[] emails)
+        {
+            var hashset = new HashSet<string>();
+
+            foreach (var email in emails)
+            {
+                var splitedEmail = email.Split('@');
+                var domaiName = splitedEmail[1];
+                var userName = splitedEmail[0];
+                var shapedUserName = userName.Remove('.');
+
+            }
+            return 0;
+        }
+
     }
 }
