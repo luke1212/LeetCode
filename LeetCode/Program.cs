38285b22d09c912fe9cc46d3238571368d0912a2
Luke_Sun <luke890602@gmail.com>
2019-02-19 13:35:52 -0600

    finished

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -8,11 +8,8 @@ namespace LeetCode
         public static void Main(string[] args)
         {
             var s = "hello,world";
-            var s1 = s.Remove(',');
-            foreach (var s2 in s1)
-            {
-                Console.WriteLine(s2);
-            }
+            Console.WriteLine(s);
+
         }
 
         // problem 3
@@ -24,6 +21,9 @@ namespace LeetCode
 
         public int NumUniqueEmails(string[] emails)
         {
+            if (emails == null || emails.Length == 0)
+                return 0;
+
             var hashset = new HashSet<string>();
 
             foreach (var email in emails)
@@ -31,10 +31,13 @@ namespace LeetCode
                 var splitedEmail = email.Split('@');
                 var domaiName = splitedEmail[1];
                 var userName = splitedEmail[0];
-                var shapedUserName = userName.Remove('.');
-
+                var shapedUserName = userName.Split('+');
+                var splittedUserName = shapedUserName[0];
+                var splitWithDot = splittedUserName.Split('.');
+                var finalUserName = String.Join("", splitWithDot);
+                hashset.Add(userName + "@" + domaiName);
             }
-            return 0;
+            return hashset.Count;
         }
 
     }
